83d9f7e43c108e5959bd4373fbd3b740d60c9523
Денис <archness@MacBook-Air-Denis.local>
2020-10-24 20:19:10 +0300

    my first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// let obj = {
+//     name: "Anton",
+//     age: 23,
+//     mass:["s","ds","dsd"],
+//     isTrue: true,
+//     welcome: function(){
+//         console.log("welcome");
+//     }
+// }
+// console.log(obj.mass[3] = "s");
+// console.log(obj.mass);
+// let mas = [1,2,3]
+// console.log(mas);
+// mas[mas.length] = 4
+// console.log(mas);
